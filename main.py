9bf90981455d3fc3a10ea786888e6591afb6e309
KasiaMik <80251673+KasiaMik@users.noreply.github.com>
2022-09-17 19:41:31 +0200

    Select columns by specific type

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ df.drop('Rank', 1, inplace = True)
 df.reset_index(inplace = True)
 df.drop('index', 1, inplace = True)
 
+"""
+      col_A   col_B
+0	C	3
+1	D	4
+2	B	2
+3	A	1
+"""
+
 # EXCERCISE 2
 
 new_column = ["P", "Q", "R", "S"]
@@ -21,12 +29,20 @@ insert_position = 1
 
 df.insert(insert_position, "Col_C", new_column)
 
-print(df.head())
+# EXCERCISE 3
+
+df2 = pd.DataFrame([["A", 1, True], ["B", 2, False],
+                   ["C", 3, False], ["D", 4, True]], 
+                  columns=["col_A", "col_B", "col_C"])
+
+dt_type = "bool"
+
+print(df2.select_dtypes(dt_type))
 
 """
-      col_A   col_B
-0	C	3
-1	D	4
-2	B	2
-3	A	1
+	col_C
+0	True
+1	False
+2	False
+3	True
 """
